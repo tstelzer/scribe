a1b4b20302d366ba5fa7b9f84dfed8619d895ee2
Timm Stelzer <timmstelzer@gmail.com>
2019-01-24 22:31:01 +0100

    Move todos to trello.

---
+++ typings/dummy/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'markdown-it-abbr';
+declare module 'markdown-it-toc-done-right';
+declare module 'markdown-it-attrs';
+declare module 'markdown-it-prism';
+declare module 'prismjs/components/index';
