8ebe0d1637f72a964aebcc50137a197bc9248662
Timm Stelzer <timmstelzer@gmail.com>
2019-02-24 20:28:55 +0100

    Update deps.

--- typings/dummy/index.d.ts
+++ typings/dummy/index.d.ts
@@ -3,3 +3,5 @@ declare module 'markdown-it-toc-done-right';
 declare module 'markdown-it-attrs';
 declare module 'markdown-it-prism';
 declare module 'prismjs/components/index';
+// FIXME: postcss Plugins are incorrectly typed
+declare module 'postcss';
